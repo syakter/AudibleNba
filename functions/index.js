4ce8ef56add051329647b44e3d37b3d66110f40d
syakter <syakter@ucsc.edu>
2018-07-14 04:22:50 -0700

    fixed indentation

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
 // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
 // for Dialogflow fulfillment library docs, samples, and to report issues
 'use strict';
- 
+
 const functions = require('firebase-functions');
-const {WebhookClient} = require('dialogflow-fulfillment');
-const {Card, Suggestion} = require('dialogflow-fulfillment');
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { Card, Suggestion } = require('dialogflow-fulfillment');
 
 var admin = require("firebase-admin");
 
@@ -19,55 +19,58 @@ admin.initializeApp({
 
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
- 
+
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
- 
+
   function welcome(agent) {
     agent.add(`Welcome to my agent! Trey, you connected to a welcome cloud function `);
-	
-//	var db = admin.database();
-//var ref = db.ref("usersbro");
-//var usersRef = ref.child("users");
-//usersRef.set({
-//  alanisawesome: {
-//    date_of_birth: "June 23, 1912",
-//    full_name: "Alan Turing"
- // },
- // gracehop: {
-  //  date_of_birth: "December 9, 1906",
-   // full_name: "Grace Hopper"
-  //}
-//});
+
+    //	var db = admin.database();
+    //var ref = db.ref("usersbro");
+    //var usersRef = ref.child("users");
+    //usersRef.set({
+    //  alanisawesome: {
+    //    date_of_birth: "June 23, 1912",
+    //    full_name: "Alan Turing"
+    // },
+    // gracehop: {
+    //  date_of_birth: "December 9, 1906",
+    // full_name: "Grace Hopper"
+    //}
+    //});
 
 
 
 
-// Get a database reference to our posts
-var db = admin.database();
-var ref = db.ref("usersbro");
-var usersRef = ref.child("users");
+    // Get a database reference to our posts
+    var db = admin.database();
+    var ref = db.ref("usersbro");
+    var usersRef = ref.child("users");
 
-// Attach an asynchronous callback to read the data at our posts reference
-usersRef.on("value", function(snapshot) {
-snapshot.forEach(function(childSnap){
-  console.log(childSnap.val().full_name+childSnap.key);
-	agent.add(childSnap.val().full_name);
-});
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
+    var league = db.ref("league");
+    var standard = league.child("standard");
+
+    // Attach an asynchronous callback to read the data at our posts reference
+    standard.on("value", (snapshot) => {
+      snapshot.forEach((playerInt) => {
+        console.log(playerInt.val().full_name + playerInt.key);
+        agent.add(playerInt.val().full_name);
+      });
+    }, (errorObject) => {
+      console.log("The read failed: " + errorObject.code);
+    });
 
 
 
   }
- 
+
   function fallback(agent) {
     agent.add(`I didn't understand`);
     agent.add(`I'm sorry, can you try again?`);
-}
+  }
 
   let intentMap = new Map();
   intentMap.set('Default Welcome Intent', welcome);
